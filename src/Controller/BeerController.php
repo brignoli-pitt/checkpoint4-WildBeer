51882f0ecd900f032a2043900678c896dcdbb40e
brignoli-pitt <pierre.brignoli@gmail.com>
2023-02-01 23:17:38 +0100

    add CRUD for Beer, Category and User

--- src/Controller/BeerController.php
+++ src/Controller/BeerController.php
@@ -2,17 +2,77 @@
 
 namespace App\Controller;
 
+use App\Entity\Beer;
+use App\Form\BeerType;
+use App\Repository\BeerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/beer')]
 class BeerController extends AbstractController
 {
-    #[Route('/beer', name: 'app_beer')]
-    public function index(): Response
+    #[Route('/', name: 'app_beer_index', methods: ['GET'])]
+    public function index(BeerRepository $beerRepository): Response
     {
         return $this->render('beer/index.html.twig', [
-            'controller_name' => 'BeerController',
+            'beers' => $beerRepository->findAll(),
         ]);
+    }
+
+    #[Route('/new', name: 'app_beer_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, BeerRepository $beerRepository): Response
+    {
+        $beer = new Beer();
+        $form = $this->createForm(BeerType::class, $beer);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $beerRepository->save($beer, true);
+
+            return $this->redirectToRoute('app_beer_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('beer/new.html.twig', [
+            'beer' => $beer,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_beer_show', methods: ['GET'])]
+    public function show(Beer $beer): Response
+    {
+        return $this->render('beer/show.html.twig', [
+            'beer' => $beer,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_beer_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Beer $beer, BeerRepository $beerRepository): Response
+    {
+        $form = $this->createForm(BeerType::class, $beer);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $beerRepository->save($beer, true);
+
+            return $this->redirectToRoute('app_beer_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('beer/edit.html.twig', [
+            'beer' => $beer,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_beer_delete', methods: ['POST'])]
+    public function delete(Request $request, Beer $beer, BeerRepository $beerRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$beer->getId(), $request->request->get('_token'))) {
+            $beerRepository->remove($beer, true);
+        }
+
+        return $this->redirectToRoute('app_beer_index', [], Response::HTTP_SEE_OTHER);
     }
 }
