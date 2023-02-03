7078a52d5949f442b09bb66b8eee083fee671063
brignoli-pitt <pierre.brignoli@gmail.com>
2023-02-03 15:33:05 +0100

    add nav

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -149,6 +149,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
+    public function getFullName(): ?string
+    {
+        return $this->lastname . ' ' . $this->firstname;
+    }
+
     public function getAvatar(): ?string
     {
         return $this->avatar;
